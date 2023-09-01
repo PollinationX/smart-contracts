e95a2c62f209fdcba5ac0c2291d9e622185dfa6c
David Tacer <david.tacer@data-vallis.com>
2023-09-01 09:17:03 +0200

    beresheetEVM

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,6 +11,10 @@ module.exports = {
     }
   },
   networks: {
+    sepolia: {
+      url: "https://rpc2.sepolia.org",
+      accounts: [process.env.PRIVATE_KEY]
+    },
     mumbai: {
       url: process.env.TESTNET_RPC,
       accounts: [process.env.PRIVATE_KEY]
@@ -20,18 +24,32 @@ module.exports = {
       chainId: 10243,
       accounts: [process.env.PRIVATE_KEY]
     },
+    beresheetEVM: {
+      url: 'https://beresheet-evm.jelliedowl.net',
+      chainId: 2022,
+      accounts: [process.env.PRIVATE_KEY]
+    },
   },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY
-  },
-  customChains: [
-    {
-      network: "artheraTestnet",
-      chainId: 10243,
-      urls: {
-        apiURL: "https://explorer-test.arthera.net/api",
-        browserURL: "https://explorer-test.arthera.net"
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+    customChains: [
+      {
+        network: "artheraTestnet",
+        chainId: 10243,
+        urls: {
+          apiURL: "https://explorer-test.arthera.net/api",
+          browserURL: "https://explorer-test.arthera.net"
+        }
+      },
+      {
+        network: "beresheetEVM",
+        chainId: 2022,
+        urls: {
+          apiURL: "https://testnet.edgscan.live/api",
+          browserURL: "https://testnet.edgscan.live"
+        }
       }
-    }
-  ]
+    ]
+  },
+
 };
